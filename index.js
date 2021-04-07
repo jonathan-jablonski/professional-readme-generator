671b46b111170cb184016f5b46a116dbf49c1649
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-04-07 11:01:16 -0500

    Added to questions array

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do we install your project?'
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'How should we use your project?'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who contributed to this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How should we test this project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
